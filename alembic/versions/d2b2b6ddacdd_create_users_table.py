6b3a7124acb276c90ad4b99329bbf5c987d9fa66
LeulBerhanu <leulberhanua@gmail.com>
2026-08-12 22:14:37 +0300

    feat: Create users table migration
    - Added a new Alembic migration script to create the 'users' table with necessary fields including id, email, hashed_password, is_active, and created_at.
    - Implemented an index on the email field to enforce uniqueness.
    - Included downgrade functionality to remove the users table and its index.

--- alembic/versions/d2b2b6ddacdd_create_users_table.py
+++ alembic/versions/d2b2b6ddacdd_create_users_table.py
@@ -1,8 +1,8 @@
 """create users table
 
-Revision ID: 98e45e671311
+Revision ID: d2b2b6ddacdd
 Revises: 
-Create Date: 2026-08-10 20:27:54.464234
+Create Date: 2026-08-12 22:08:52.637375
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '98e45e671311'
+revision: str = 'd2b2b6ddacdd'
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
