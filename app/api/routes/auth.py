76b55001e5b5a699e0a17966c10f8711cc4cd9d8
LeulBerhanu <leulberhanua@gmail.com>
2026-08-13 18:06:31 +0300

    refactor: Update login functionality to use OAuth2PasswordRequestForm
    - Modified the login endpoint to accept OAuth2PasswordRequestForm instead of a custom LoginRequest schema, streamlining the authentication process.
    - Refactored the login_user service to accept email and password as separate parameters, enhancing clarity and usability.
    - Improved dependency injection for form data in the login function, aligning with FastAPI best practices.

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -1,12 +1,14 @@
+from typing import Annotated
 from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlmodel import Session
 from app.db.session import get_session
-from app.schemas.auth import LoginRequest, Token
+from app.schemas.auth import Token
 from app.services.auth import login_user
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 @router.post("/login", response_model=Token)
-def login(request: LoginRequest, session: Session = Depends(get_session)):
-    return login_user(session, request)
+def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], session: Session = Depends(get_session)):
+    return login_user(session, form_data.username, form_data.password)
      
--- app/services/auth.py
+++ app/services/auth.py
@@ -1,18 +1,16 @@
-
-
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlmodel import Session
 from app.core.exceptions import BadRequestError
 from app.core.security import create_access_token, verify_password
-from app.schemas.auth import LoginRequest
 from app.services.user import get_user_by_email
 
 
-def login_user(session: Session, request: LoginRequest) -> dict[str, str]:
-    user = get_user_by_email(session, request.email)
+def login_user(session: Session, email: str, password: str) -> dict[str, str]:
+    user = get_user_by_email(session, email)
     
     if not user:
         raise BadRequestError("Invalid Email or Password")
-    if not verify_password(request.password, user.hashed_password):
+    if not verify_password(password, user.hashed_password):
         raise BadRequestError("Invalid Email or Password")
     
     token = create_access_token(user.id)
