e2b705ed0d38a284bd41d852dfd0fe026599fa7a
LeulBerhanu <leulberhanua@gmail.com>
2026-08-13 18:05:42 +0300

    feat: Add dependency for current user retrieval in API
    - Introduced a new `deps.py` file to manage dependencies for FastAPI.
    - Implemented `get_current_user` function to retrieve the current user based on the provided OAuth2 token, enhancing user authentication handling.
    - Integrated error handling for invalid tokens and non-existent users, improving robustness of the authentication process.

---
+++ app/api/deps.py
@@ -0,0 +1,27 @@
+
+from typing import Annotated
+from uuid import UUID
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+from sqlmodel import Session
+from app.core.exceptions import BadRequestError
+from app.core.security import decode_access_token
+from app.db.session import get_session
+from app.models.user import User
+from app.repositories.user import UserRepository
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
+
+def get_current_user(session: Annotated[Session, Depends(get_session)], token: Annotated[str, Depends(oauth2_scheme)]) -> User:
+    sub = decode_access_token(token)
+    
+    if not sub:
+        raise BadRequestError("Invalid Token")
+        
+    repo = UserRepository(session)
+    user = repo.get_by_id(sub)
+
+    if not user:
+        raise BadRequestError("User not found")
+    return user
