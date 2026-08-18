ffb65a1dbc152d8ca181f8e587bfbb6443ef064d
LeulBerhanu <leulberhanua@gmail.com>
2026-08-18 11:44:18 +0300

    refactor: Update token validation in get_current_user function
    - Replaced BadRequestError with UnauthorizedError for invalid token handling in the get_current_user function, improving clarity in authentication error responses.
    - Adjusted error handling to raise UnauthorizedError when the user is not found, enhancing security and user feedback.

--- app/api/deps.py
+++ app/api/deps.py
@@ -4,7 +4,7 @@ from uuid import UUID
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from sqlmodel import Session
-from app.core.exceptions import BadRequestError
+from app.core.exceptions import UnauthorizedError
 from app.core.security import decode_access_token
 from app.db.session import get_session
 from app.models.user import User
@@ -16,12 +16,9 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
 def get_current_user(session: Annotated[Session, Depends(get_session)], token: Annotated[str, Depends(oauth2_scheme)]) -> User:
     sub = decode_access_token(token)
     
-    if not sub:
-        raise BadRequestError("Invalid Token")
-        
     repo = UserRepository(session)
     user = repo.get_by_id(sub)
 
     if not user:
-        raise BadRequestError("User not found")
+        raise UnauthorizedError("Invalid Token")
     return user
