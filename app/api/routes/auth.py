5c0521d8be36583caa90266be71fa1629939df9e
LeulBerhanu <leulberhanua@gmail.com>
2026-08-18 20:33:00 +0300

    refactor: Update login function to use async session for improved performance
    - Changed the session dependency in the login function from SyncSession to AsyncSession, aligning with the async architecture of the application.
    - Updated the session retrieval method to get_async_session, enhancing compatibility with asynchronous database operations.

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -1,14 +1,14 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
-from sqlmodel import Session
-from app.db.session import get_session
+from sqlmodel.ext.asyncio.session import AsyncSession
+from app.db.session import get_async_session
 from app.schemas.auth import Token
 from app.services.auth import login_user
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 @router.post("/login", response_model=Token)
-def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], session: Session = Depends(get_session)):
+def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], session: AsyncSession = Depends(get_async_session)):
     return login_user(session, form_data.username, form_data.password)
      
